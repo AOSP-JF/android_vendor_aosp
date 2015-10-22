JDCTeam/android_vendor_jdc

common: add eMMC trim

config/common.mk
@@ -10,6 +10,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/jdc/prebuilt/common/bin/format.sh:install/bin/format.sh
 
+# eMMC trim
+PRODUCT_COPY_FILES += \
+    vendor/jdc/prebuilt/common/bin/emmc_trim:system/bin/emmc_trim
+
 # For keyboard gesture typing
 PRODUCT_COPY_FILES += \
     vendor/jdc/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so

AOSP-JF/android_vendor_aosp

 Conflicts:
	config/common.mk

config/common.mk
@@ -70,6 +70,9 @@ PRODUCT_PACKAGES += \
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.adb.secure=0
     
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/bin/emmc_trim:system/bin/emmc_trim
+
 # For keyboard gesture typing
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so